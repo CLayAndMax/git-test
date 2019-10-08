d0220ae0150b29eeed7be50ec8331480093926aa
CLayAdnMax <1679106670@qq.com>
2019-10-08 15:31:31 +0800

    把本地工程添加到Git

---
+++ git-test-project/src/com/oracle/FirstClass.java
@@ -0,0 +1,13 @@
+package com.oracle;
+
+/**
+ * @Auther: a
+ * @Date: 2019/10/8 15:01
+ * @Description:
+ */
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
